c3b5cfdc8e9882f4e562a5027116d2a4775f0838
soniamouna <66943124+soniamouna@users.noreply.github.com>
2022-08-25 01:17:55 +0200

    Seeder créé

--- api/database/seeders/PairsSeeder.php
+++ api/database/seeders/PairsSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Pairs;
 
 class PairsSeeder extends Seeder
 {
@@ -14,6 +15,17 @@ class PairsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Pairs::factory()->create([
+            'from'=>'EUR',
+            'to'=>'USD',
+            'count'=>0,
+            'rate'=>0.99,
+        ]);
+        Pairs::factory()->create([
+            'from'=>'USD',
+            'to'=>'EUR',
+            'count'=>0,
+            'rate'=>1.1,
+        ]);
     }
 }
