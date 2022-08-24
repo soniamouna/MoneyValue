9361bb17545dc7eae773418663fc9f8b61dc6bc0
soniamouna <66943124+soniamouna@users.noreply.github.com>
2022-08-25 00:29:44 +0200

    Ajout des fichiers Controller, Resource, Migrations, Factory, Seeder

---
+++ api/app/Http/Controllers/Api/PairsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Pairs;
+use Illuminate\Http\Request;
+
+class PairsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Pairs  $pairs
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pairs $pairs)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Pairs  $pairs
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pairs $pairs)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Pairs  $pairs
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pairs $pairs)
+    {
+        //
+    }
+}
